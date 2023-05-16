bef72f9dfd38e088f33feba369eaea302ec37464
Yersultan <ersultansansyzbaj@gmail.com>
2023-05-16 14:48:21 +0600

    REST API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { Main } from './components/Main';
+import { Controls } from './components/Controls';
 
 function App() {
   return (
     <BrowserRouter>
-   <Header/>
+  <Header/>
+     {/* <Main>
+     <Controls/>
+     </Main> */}
    </BrowserRouter>
     );
 }
